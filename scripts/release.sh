d45bf8d7fc4edce6b96663a3255b59e3f36bce2e
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2023-03-29 16:59:20 -0700

    Save releases to a directory

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,8 @@ version=$(echo $last_version | awk -F. '/[0-9]+\./{$NF++;print}' OFS=.)
 echo "Next version: $version"
 
 echo "Publishing release"
-cp "fyne-cross/dist/windows-amd64/TikTok Archiver.exe.zip" "TikTok Archiver-$version-windows-amd64.zip"
-cp "fyne-cross/dist/darwin-amd64/TikTok Archiver.zip" "TikTok Archiver-$version-macos-amd64.zip"
-cp "fyne-cross/dist/darwin-arm64/TikTok Archiver.zip" "TikTok Archiver-$version-macos-arm64.zip"
-gh release create $version "TikTok Archiver-$version-windows-amd64.zip#Windows (64-bit)" "TikTok Archiver-$version-macos-amd64.zip#macOS (Intel)" "TikTok Archiver-$version-macos-arm64.zip#macOS (M1)" --title "TikTok Archiver $version" --generate-notes
+mkdir -p release/
+cp "fyne-cross/dist/windows-amd64/TikTok Archiver.exe.zip" "release/TikTok Archiver-$version-windows-amd64.zip"
+cp "fyne-cross/dist/darwin-amd64/TikTok Archiver.zip" "release/TikTok Archiver-$version-macos-amd64.zip"
+cp "fyne-cross/dist/darwin-arm64/TikTok Archiver.zip" "release/TikTok Archiver-$version-macos-arm64.zip"
+gh release create $version "release/TikTok Archiver-$version-windows-amd64.zip#Windows (64-bit)" "release/TikTok Archiver-$version-macos-amd64.zip#macOS (Intel)" "release/TikTok Archiver-$version-macos-arm64.zip#macOS (M1)" --title "TikTok Archiver $version" --generate-notes
